5c17f8ee942bdc0dc1c98341e3fd65a9345e52e0
Raja Soundaramourty <rajasoun@icloud.com>
2021-04-27 13:44:08 +0530

    Initial CheckIn

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Next.js 100</title>
+      </Head>
+      <div>
+        Next.js 100
+      </div>
+    </>
+  )
+}
+
+export default Index
